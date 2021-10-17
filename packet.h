4bd9a6f3aa9899864dc9e080cf5ace53f77722c5
Da Ma <79146609+Da-Dylan-Ma@users.noreply.github.com>
2021-10-17 08:00:16 +0800

    Update stringToPacket(...)

--- packet.h
+++ packet.h
@@ -9,4 +9,47 @@ struct packet{
     char filedata[1000];
 };
 
+
+void stringToPacket(char* src, struct Packet* dest){
+    int start = 0, end = 0;
+    char* buf;
+
+    // Extract total_fragment number from first Position
+    while(src[end] != ':') end++;
+    buf = malloc((end - start) * sizeof(char) + 1);
+    memcpy(buf, &src[start], end - start);
+    dest->total_frag = atoi(buf);
+    end++;
+    start = end;
+    free(buf);
+
+    // Extract frag_no number from second Position
+    while(src[end] != ':') end++;
+    buf = malloc((end - start) * sizeof(char) + 1);
+    memcpy(buf, &src[start], end - start);
+    dest->frag_no = atoi(buf);
+    end++;
+    start = end;
+    free(buf);
+
+    // Extract size from third Position
+    while(src[end] != ':') end++;
+    buf = malloc((end - start) * sizeof(char) + 1);
+    memcpy(buf, &src[start], end - start);
+    dest->size = atoi(buf);
+    end++;
+    start = end;
+    free(buf);
+
+    // Extract file name from fourth Position
+    while(src[end] != ':') end++;
+    dest->filename = malloc((end - start) * sizeof(char) + 1);
+    memcpy(dest->filename, &src[start], end - start);
+    end++;
+    start = end;
+
+    // Lastly, Extract file data from Position left
+    memcpy(dest->filedata, &src[start], dest->size);
+}
+
 #endif
