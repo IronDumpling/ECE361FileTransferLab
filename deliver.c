c372f8247a7a89a2de76cb2e9975fb718e76eac2
Chuyue Zhang <70104294+IronDumpling@users.noreply.github.com>
2021-10-17 02:43:57 -0400

    Resending Procedure of client

--- deliver.c
+++ deliver.c
@@ -1,6 +1,7 @@
 // Basic
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 #include <unistd.h>
 // Socket
@@ -8,10 +9,24 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <libgen.h>
 // OS
 #include <fcntl.h>
+// Others
+#include <time.h>
+#include "packet.h"
 
-// Function 1. Client Function
+#define ALIVE 32
+
+// Function Prototypes
+bool getFile(char*);
+void requestACK(int clientSocket, struct sockaddr_in*);
+void sendFile(int, struct sockaddr_in*, char*);
+void receiveFile(int, struct sockaddr_in*);
+void packetToString(char*, struct packet*);
+void stringToPacket(char*, struct packet*);
+
+// Function 1. Client Main Function
 int main(int argc, char **argv)
 {
     // Step 0. Get Input
@@ -42,34 +57,177 @@ int main(int argc, char **argv)
     }
 
     // Step 4. User Input File Name
-    // Output Guidance
-    printf("Please Input File Name: ");
-    // Get File Name
-    char prefix[4], fileName[256];
-    scanf("%s %s", prefix, fileName);
-
-    // Step 6. Check if the file exists
-    int fd = open(fileName, O_RDWR | O_NOCTTY | O_NDELAY);
-    if(fd < 0){
+    char fileName[256];
+    if(!getFile(fileName)){
         perror("This File Does Not Exist");
         exit(1);
     }
 
-    // Step 7. Send Message to the server
-    char message[] = "ftp";
-    socklen_t addressLength = sizeof(struct sockaddr);
-    int snd = sendto(clientSocket, message, strlen(message) + 1, MSG_NOSIGNAL,
-                      (struct sockaddr*)&serverAddress, addressLength);
+    // Step 5. Send File to the server
+    sendFile(clientSocket, &serverAddress, fileName);
 
-    if(snd < 0){
-        perror("Send Message Failed");
+    // Step 6. Receive Message from the server
+    receiveFile(clientSocket, &serverAddress);
+
+    // Step 7. Close all
+    close(clientSocket);
+    return 0;
+}
+
+// Function 2. Get File
+bool getFile(char fileName[]){
+    // Output Guidance
+    printf("Please Input File Name: ");
+
+    // Get File Name
+    char prefix[4];
+    scanf("%s %s", prefix, fileName);
+
+    // Check if the file exists
+    int fd = open(fileName, O_RDWR | O_NOCTTY | O_NDELAY);
+    if(fd < 0){
+        return false;
+    }
+    return true;
+}
+
+// Function 3. Send File Function
+void sendFile(int clientSocket, struct sockaddr_in *serverAddress, char fileName[]){
+    // Step 1. Wave Hand
+    // Request Acknowledge
+    requestACK(clientSocket, serverAddress);
+
+    // Receive File
+    receiveFile(clientSocket, serverAddress);
+
+    // Step 2. Preparation
+    // Initialize a File Struct
+    FILE *filePtr = fopen(fileName, "rb");
+
+    // Get Size of file
+    int fsk = fseek(filePtr, 0, SEEK_END);
+    // Error Handling
+    if(fsk){
+        perror("Seek File Failed");
         exit(1);
     }
 
-    // Step 8. Receive Message from the server
+    long fileSize = ftell(filePtr);
+    // Let filePtr point to the head of the file again
+    rewind(filePtr);
+
+    // Step 3. Read File
+    // Create a Char Array of the File
+    char file[fileSize];
+    fread(file, sizeof(char), fileSize, filePtr);
+    fclose(filePtr);
+
+    // Get the number of fragments
+    long fragNum = fileSize/DataSize;
+    // Remainder
+    fragNum += (fileSize % DataSize != 0) ? 1 : 0;
+
+    // Send Multiple Fragments
+    // Initialize packets
+    char **packets = malloc(sizeof(char*) * fragNum);
+    char receBuff[DataSize];
+
+    for(int frag = 0; frag < fragNum; ++frag){
+        struct packet currPacket;
+
+        // Assign Info
+        currPacket.total_frag = fragNum;
+        currPacket.frag_no = frag;
+        currPacket.size = (frag == (fragNum - 1)) ? fileSize % DataSize : DataSize;
+        currPacket.filename = basename(fileName);
+
+        // Copy File
+        memcpy(currPacket.filedata, file + (frag * DataSize), currPacket.size);
+
+        // Convert Packet to String
+        packets[frag] = malloc(PacketSize * sizeof(char));
+        packetToString(packets[frag], &currPacket);
+    }
+
+    struct timeval timeout;
+    timeout.tv_sec = 1;
+    timeout.tv_usec = 0;
+
+    int ssk = setsockopt(clientSocket, SOL_SOCKET, SO_RCVTIMEO, (char*)&timeout, sizeof(timeout));
+    if(ssk < 0){
+        perror("Set Socket Failed\n");
+        exit(1);
+    }
+
+    int timeSent = 0;
+    socklen_t addressLength = sizeof(struct sockaddr);
+
+    struct packet ackPacket;
+    ackPacket.filename = (char*)malloc(PacketSize * sizeof(char));
+
+
+    for(int frag = 0; frag < fragNum; ++frag){
+        ++timeSent;
+
+        // Send string packet
+        int snd = sendto(clientSocket, packets[frag], PacketSize, 0,
+                         (struct sockaddr*)serverAddress, addressLength);
+
+        if(snd < 0){
+            perror("Send Packet Failed");
+            exit(1);
+        }else{
+            printf("Send Packet %d Succeed\n", frag);
+        }
+
+        // Receive ACK
+        memset(receBuff, 0, sizeof(char) * DataSize);
+
+        int length = recvfrom(clientSocket, receBuff, DataSize, 0,
+                              (struct sockaddr*)serverAddress, &addressLength);
+
+        if(length < 0){
+            // Resend if Timeout
+            if(timeSent < ALIVE)
+                continue;
+            // Else exit
+            perror("Receive Message Failed");
+            exit(1);
+        }
+
+        stringToPacket(receBuff, &ackPacket);
+
+        // Check contents of ACK Packet
+        if(strcmp(ackPacket.filename, fileName) == 0){
+            if(ackPacket.frag_no == frag){
+                if(strcmp(ackPacket.filedata, "ACK") == 0){
+                    timeSent = 0;
+                    continue;
+                }
+            }
+        }
+
+        // Resend
+        fprintf(stderr, "ACK packet #%d not received, resending attempt #%d\n", frag, timeSent);
+        --frag;
+    }
+
+    // Free Mem
+    for(int frag = 0; frag < fragNum; ++frag){
+        free(packets[frag]);
+    }
+    free(packets);
+    free(ackPacket.filename);
+}
+
+// Function 4. Receive File Function
+void receiveFile(int clientSocket, struct sockaddr_in* serverAddress){
+    // Receive Message from the server
     char buffer[1024];
-    int length = recvfrom(clientSocket, buffer, strlen(buffer), 0,
-                          (struct sockaddr*)&serverAddress, &addressLength);
+    socklen_t addressLength = sizeof(struct sockaddr);
+
+    int length = recvfrom(clientSocket, buffer, sizeof(buffer), 0,
+                          (struct sockaddr*)serverAddress, &addressLength);
 
     if(length < 0){
         perror("Receive Message Failed");
@@ -79,8 +237,21 @@ int main(int argc, char **argv)
     if(strcmp(buffer, "yes") == 0){
         printf("A file transfer can start.\n");
     }
+}
 
-    // Step 9. Close all
-    close(clientSocket);
-    return 0;
+// Function 5. Require Acknowledge Number
+void requestACK(int clientSocket, struct sockaddr_in *serverAddress){
+    socklen_t addressLength = sizeof(struct sockaddr);
+
+    // Send Acknowledge Request
+    int snd = sendto(clientSocket, "ftp", strlen("ftp") + 1, MSG_NOSIGNAL,
+                     (struct sockaddr*)serverAddress, addressLength);
+
+    // Send File Failure
+    if(snd < 0){
+        perror("Send Request Failed");
+        exit(1);
+    }else{
+        printf("Send Request Succeed\n");
+    }
 }
--- packet.h
+++ packet.h
@@ -1,21 +1,54 @@
 #ifndef _PACKET_H
 #define _PACKET_H
 
+#define DataSize 1000
+#define PacketSize 1200
+
 struct packet{
     unsigned int total_frag;
     unsigned int frag_no;
     unsigned int size;
     char* filename;
-    char filedata[1000];
+    char filedata[DataSize];
 };
 
+// Function 6. Packet to String
+void packetToString(char destination[], struct packet* source){
+    // Initialize
+    memset(destination, '\0', PacketSize);
 
-void stringToPacket(char* src, struct Packet* dest){
+    unsigned long index;
+
+    // Add total fragment
+    sprintf(destination, "%d", source->total_frag);
+    index = strlen(destination);
+    destination[index++] = ':';
+
+    // Add fragment number
+    sprintf(destination + index, "%d", source->frag_no);
+    index = strlen(destination);
+    destination[index++] = ':';
+
+    // Add Size
+    sprintf(destination + index, "%d", source->size);
+    index = strlen(destination);
+    destination[index++] = ':';
+
+    // Add filename
+    sprintf(destination + index, "%s", source->filename);
+    index += strlen(source->filename);
+    destination[index++] = ':';
+
+    // Add file data
+    memcpy(destination + index, source->filedata, sizeof(char) * DataSize);
+}
+
+void stringToPacket(char* src, struct packet* dest) {
     int start = 0, end = 0;
-    char* buf;
+    char *buf;
 
     // Extract total_fragment number from first Position
-    while(src[end] != ':') end++;
+    while (src[end] != ':') end++;
     buf = malloc((end - start) * sizeof(char) + 1);
     memcpy(buf, &src[start], end - start);
     dest->total_frag = atoi(buf);
@@ -24,7 +57,7 @@ void stringToPacket(char* src, struct Packet* dest){
     free(buf);
 
     // Extract frag_no number from second Position
-    while(src[end] != ':') end++;
+    while (src[end] != ':') end++;
     buf = malloc((end - start) * sizeof(char) + 1);
     memcpy(buf, &src[start], end - start);
     dest->frag_no = atoi(buf);
@@ -33,7 +66,7 @@ void stringToPacket(char* src, struct Packet* dest){
     free(buf);
 
     // Extract size from third Position
-    while(src[end] != ':') end++;
+    while (src[end] != ':') end++;
     buf = malloc((end - start) * sizeof(char) + 1);
     memcpy(buf, &src[start], end - start);
     dest->size = atoi(buf);
@@ -42,14 +75,11 @@ void stringToPacket(char* src, struct Packet* dest){
     free(buf);
 
     // Extract file name from fourth Position
-    while(src[end] != ':') end++;
+    while (src[end] != ':') end++;
     dest->filename = malloc((end - start) * sizeof(char) + 1);
     memcpy(dest->filename, &src[start], end - start);
     end++;
     start = end;
-
-    // Lastly, Extract file data from Position left
-    memcpy(dest->filedata, &src[start], dest->size);
 }
 
 #endif
